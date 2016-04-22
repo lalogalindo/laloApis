26977ffd8d155eed04cf6026819677a8035bff94
Eduardo Galindo <egalindo@jobomas.com>
2016-04-22 16:04:42 -0500

    tablas actualizado

--- newTablas.js
+++ newTablas.js
@@ -1,49 +1,59 @@
 /**
  * Objeto que controla llenado, vaciado y paginado de una tabla.
- * @param {selector} selectorTabla => Selector de la tabla.
+ * @param {CSS Selector} selectorTabla => Selector de la tabla.
+ * @author Eduardo Galindo Franco <lalogalindo@gmail.com>
  */
-function TablaDinamica(selectorTabla)
-	var TablaDinamica
+function TablaDinamica(selectorTabla) {
+	var TablaDinamica,tableBody
 	if( !selectorTabla )
-		throw new Error('-> "selectorTabla" not defined')
+		throw new Error('-> Must provide selector as parameter')
 	TablaDinamica = document.querySelector(selectorTabla)
 	if( !TablaDinamica )
-		throw new Error('-> "Tabla" not found')
+		throw new Error('-> Table "'+selectorTabla+'" not found')
+
+	/**
+	 * Elimina todos los datos dentro del cuerpo de la tabla
+	 * @type {Method}
+	 * @author Eduardo Galindo Franco <lalogalindo@gmail.com>
+	 */
+	function clearBody() {
+		if( !this.body )
+			this.getBody()
+		while(this.body.children.length > 0) {
+			this.body.removeChild(this.body.firstChild)
+		}
+	}
+
+	/**
+	 * Obtiene el cuerpo de la tabla.
+	 * @type {Method}
+	 * @returns {HTMLTableSectionElement} => Etiqueta <tbody>
+	 * @author Eduardo Galindo Franco <lalogalindo@gmail.com>
+	 */
+	function getBody() {
+		if( !tableBody ){
+			var body = TablaDinamica.querySelector('tbody')
+			if( body )
+				tableBody = body
+			else
+				throw new Error('-> "tbody" not found')
+		}
+		return tableBody
+	}
 
 	Object.defineProperties(this,{
-		/**
-		 * Obtiene el cuerpo de la tabla.
-		 * @type {Method}
-		 * @returns {HTMLTableSectionElement} => Etiqueta <tbody>
-		 * @author Eduardo Galindo Franco <egalindo@jobomas.com>
-		 */
 		getBody: {
-			value: function() {
-				var body = TablaDinamica.querySelector('tbody')
-				if( body ){
-					this.body = body
-					return body
-				} else
-					throw new Error('-> "tbody" not found')
-			},
-		},
-		/**
-		 * Elimina todos los datos dentro del cuerpo de la tabla
-		 * @type {Method}
-		 * @author Eduardo Galindo Franco <egalindo@jobomas.com>
-		 */
-		clearBody: {
-			value: function() {
-				if( !this.body )
-					this.getBody()
-				while(this.body.children.length > 0) {
-					this.body.removeChild(this.body.firstChild)
-				}
+			value: function(){
+				return getBody()
 			}
 		},
+		/**
+		 * Crea atributos para el objeto asignado
+		 * @param {Object} obj  => Objeto al cual se asignarán los atributos
+		 * @param {Object} attr => Objeto con los atributos a asignar
+		 */
 		setAttributes: {
 			value: function(obj,attr) {
-				debugger
 				for (var data in attr) {
 					if (attr.hasOwnProperty(data)) {
 						if( typeof attr.data == 'object' ) {
@@ -60,31 +70,38 @@ function TablaDinamica(selectorTabla)
 		 * @param {array} items       => Es un arreglo con los valores a insertar.
 		 * @param {object} attributes => Es un objeto con los atributos de la fila.
 		 * @type {Method}
-		 * @author Eduardo Galindo Franco <egalindo@jobomas.com>
+		 * @author Eduardo Galindo Franco <lalogalindo@gmail.com>
 		 */
 		addRow: {
 			value: function(item) {
-				debugger
-				var tr = document.createElement('tr')
+				var tr = document.createElement('tr'),
+				that = this
 				if( !item )
 					throw new Error("Can't create empty Row, try with method 'addEmptyRow'")
 				else {
-					debugger
-					for (var i = 0; i < item.data.length; i++) {
-						var data = item.data[i]
-						if( data.attributes ) {
-							if( data.attributes.constructor.name === 'Object' ) {
-								for( var key in data.attributes ) {
-									if( data.attributes.hasOwnProperty(key) ) {
-										tr.setAttribute(key,data.attributes.key)
-									}
+					if( item.attributes ) {
+						if( item.attributes.constructor.name === 'Object' ) {
+							for( var key in item.attributes ) {
+								if( item.attributes.hasOwnProperty(key) ) {
+									tr.setAttribute(key,item.attributes[key])
 								}
-							} else if( attributes === 'String' ) {
-								tr.setAttributeNode( document.createAttributeNode( attributes ) )
-							} else {
-								throw new Error('-> "attributes" is ' + attributes.constructor.name)
 							}
+						} else if( attributes === 'String' ) {
+							tr.setAttributeNode( document.createAttributeNode( attributes ) )
+						} else {
+							throw new Error('-> "attributes" is ' + attributes.constructor.name)
 						}
+					}
+					for (var i = 0; i < item.items.length; i++) {
+						var data = item.items[i]
+						tr.appendChild( that.addData(data) )
+					}
+
+					try {
+						that.getBody().appendChild(tr)
+					} catch(e) {
+						var message = '-> Stack from "addRows" -> ' + e.message
+						throw new Error( message )
 					}
 				}
 			}
@@ -94,36 +111,45 @@ function TablaDinamica(selectorTabla)
 		 * @type {Method}
 		 */
 		addRows: {
-			value: function(items) {
-				debugger
-				if( items.constructor.name === 'Array' ) {
+			value: function(items,overwrite) {
+				var that = this,
+				overwrite = overwrite || false
+				if( overwrite )
+					this.clearBody()
+				Array.prototype.forEach.call(items,function(item){
 					console.time('-> Init for Cycle ::addRows')
-					for (var i = 0; i < items.length; i++) {
-						this.addRow(items[i])
-						console.log( '-> Added row ',i+1 )
-					}
+					that.addRow(item)
+					console.log( '-> Added row ',i+1 )
 					console.timeEnd('-> Init for Cycle ::addRows')
-				}
+				})
 			}
 		},
 		addData: {
-			value: function(attributes) {
+			value: function(item) {
 				var td = document.createElement('td')
-				for(key in attributes) {
-					if( attributes.hasOwnProperty(key) ) {
-						if( key !== 'data' ) {
-							td.setAttribute( key, attributes.key )
-						} else {
-							td.appendChild( this.generateTableData( attributes.key ) )
+				if( item.attributes ) {
+					for(key in item.attributes) {
+						if( item.attributes.hasOwnProperty(key) ) {
+							if( key !== 'data' ) {
+								td.setAttribute( key, item.attributes[key] )
+							} else {
+								console.log('jamon')
+							}
 						}
 					}
 				}
+				if( !item.item ) {
+					throw new Error("-> 'Item' ")
+				}
+				td.appendChild( this.generateTableData( item.item ) )
+				return td
 			}
 		},
-		createTableData: {
+		generateTableData: {
 			value: function(data) {
 				var p = document.createElement('p')
-				p.textContent = data
+				//if( typeof data === 'String' || typeof data === 'Number')
+					p.textContent = data
 				return p
 			},
 			writable: true,
